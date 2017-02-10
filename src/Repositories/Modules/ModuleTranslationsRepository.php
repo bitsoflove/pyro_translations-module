abd16e4fc8a0a81028d3c47944c31d7d452a128e
Pepijn Olivier <olivier.pepijn@gmail.com>
2017-02-10 12:44:49 +0100

    Using custom TranslatorServiceProvider

--- src/Repositories/Modules/ModuleTranslationsRepository.php
+++ src/Repositories/Modules/ModuleTranslationsRepository.php
@@ -14,6 +14,42 @@ class ModuleTranslationsRepository implements TranslationRepositoryInterface
         $this->dbTranslationsRepo = app(DatabaseTranslationRepository::class);
     }
 
+
+    /**
+     * todo later: fallback and replace
+     */
+    public function get($key, array $replace = [], $locale = null, $fallback = true, $fileMatches=null) {
+
+        // 1. we already got our file matches so just query the database
+        $dbMatches = $this->dbTranslationsRepo->get($key, $replace, $locale, $fallback);
+
+        // 2. can we find an exact match ? return exact match
+        if(isset($dbMatches[$key])) {
+            return $dbMatches[$key];
+        }
+
+        // 3. no exact match found on the database level ?
+        //    3a. if $fileMatches is not empty and not an array, we do have an exact file match on the file driver
+        if(!empty($fileMatches) && !is_array($fileMatches)) {
+            return $fileMatches;
+        }
+
+        //    3b. else return assoc array (merged)
+        if(!is_array($fileMatches)) {
+            $fileMatches = [];
+        }
+        if(!is_array($dbMatches)) {
+            $dbMatches = [];
+        }
+
+        $merged = $this->mergeFileTranslationsWithDatabaseTranslations($fileMatches, $dbMatches);
+        return empty($merged) ? null : $merged;
+    }
+
+
+    /**
+     * This method is mainly here to support the admin view
+     */
     public function getAddonTranslations($addonNamespace, $locale, array $parameters=[]) {
         $fileTranslations = $this->fileTranslationsRepo->getAddonTranslations($addonNamespace, $locale, $parameters);
         $databaseTranslations = $this->dbTranslationsRepo->getAddonTranslations($addonNamespace, $locale, $parameters);

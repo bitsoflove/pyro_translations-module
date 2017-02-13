02ed048a0e42b1c64f53d2b86218415c2eb4a7dc
Pepijn Olivier <olivier.pepijn@gmail.com>
2017-02-13 13:55:38 +0100

    Update TranslatorServiceProvider.php

--- src/Translator/TranslatorServiceProvider.php
+++ src/Translator/TranslatorServiceProvider.php
@@ -1,5 +1,7 @@
 <?php namespace Bitsoflove\TranslationsModule\Translator;
 
+use Bitsoflove\TranslationsModule\TranslationsModule;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Translation\TranslationServiceProvider;
 use Bitsoflove\TranslationsModule\Translator\Translator as BolTranslator;
 
@@ -7,22 +9,32 @@ class TranslatorServiceProvider extends TranslationServiceProvider
 {
     public function boot()
     {
-        $this->app->offsetUnset('translation.loader');
-        $this->app->offsetUnset('translator');
+        try {
 
-        $this->registerLoader();
+            $module = app(TranslationsModule::class);
+            if(!$module->isInstalled()) {
+                return false;
+            }
 
-        $this->app->singleton('translator', function ($app) {
-            $loader = $app['translation.loader'];
+            $this->app->offsetUnset('translation.loader');
+            $this->app->offsetUnset('translator');
 
-            // When registering the translator component, we'll need to set the default
-            // locale as well as the fallback locale. So, we'll grab the application
-            // configuration so we can easily get both of these values from there.
-            $locale = $app['config']['app.locale'];
+            $this->registerLoader();
 
-            $trans = new BolTranslator($loader, $locale);
-            $trans->setFallback($app['config']['app.fallback_locale']);
-            return $trans;
-        });
+            $this->app->singleton('translator', function ($app) {
+                $loader = $app['translation.loader'];
+
+                // When registering the translator component, we'll need to set the default
+                // locale as well as the fallback locale. So, we'll grab the application
+                // configuration so we can easily get both of these values from there.
+                $locale = $app['config']['app.locale'];
+
+                $trans = new BolTranslator($loader, $locale);
+                $trans->setFallback($app['config']['app.fallback_locale']);
+                return $trans;
+            });
+        } catch(\Exception $e) {
+            Log::critical($e);
+        }
     }
 }
